a60eaef322337495e572f4c75ad580ad9a10530c
Rutvik Gondekar <gondekarrutvik@gmail.com>
2024-10-18 23:08:55 +0530

    Small commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -94,6 +94,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1];
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
+    console.log(token);
   }
 
   if (!token) {
